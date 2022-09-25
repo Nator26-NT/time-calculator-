08de1c091c8a04f0e4f37196fac84f9e72a4271e
Nator26-NT <111059939+Nator26-NT@users.noreply.github.com>
2022-09-25 21:24:48 +0200

    Add files via upload

---
+++ TIME-CALCULATOR.py
@@ -0,0 +1,17 @@
+class Time_Calculator :
+	x = 0
+	
+def Time(_Calculator):
+        _Calculator.x = ("15:26 ")
+        print('current_time ', _Calculator.x)
+		
+		
+		
+time = Time_Calculator
+		
+		
+		
+current_time = input((" what is the time [Format HH:MM] _________" ))
+
+print("THE TIME IN UR LOCAL AREA IS >>   " +current_time)
+		
